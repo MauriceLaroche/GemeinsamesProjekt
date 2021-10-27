ee3fd4d86f6dcf78f0ab9fa0d50fa2ebeb16c976
mlaroche <mlaroche@ITEM-S112908.emea.msad.sopra>
2021-10-27 11:35:15 +0200

    Philipp ist unser Leistungsträger

--- Maurice/src/Main.java
+++ Maurice/src/Main.java
@@ -2,7 +2,7 @@
 public class Main {
 
 	public static void main(String[] args) {
-		System.out.println("Guten Morgen");
+		System.out.println("Philipp ist unser Leistungsträger haha  ha haha");
 
 	}
 
